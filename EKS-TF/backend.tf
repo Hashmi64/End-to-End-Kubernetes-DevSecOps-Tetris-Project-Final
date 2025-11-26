cfc9f46425f60c638d31dcdb4901a0d3cc57935c
Hashmi64 <mosharib11@gmail.com>
2025-11-26 13:54:45 +0530

    updated

--- EKS-TF/backend.tf
+++ EKS-TF/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket         = "my-ews-baket1"
+    bucket         = "my-jenkins-eks-v2"
     region         = "ap-south-1"
     key            = "End-to-End-Kubernetes-DevSecOps-Tetris-Project/EKS-TF/terraform.tfstate"
     dynamodb_table = "lockfile"
